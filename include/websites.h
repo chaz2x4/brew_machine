308610b5fc18fe7c7245728f496c25b7bf23d42c
Chaz George <chaz2x4@users.noreply.github.com>
2021-06-20 12:07:53 -0700

    Add bootstrap for design

--- include/websites.h
+++ include/websites.h
@@ -198,5 +198,7 @@ const char* indexHtml = "<html>\n"
 "    <script src=\"https://cdn.jsdelivr.net/npm/chart.js@3.3.2\"></script>\n"
 "    <script src=\"https://cdn.jsdelivr.net/npm/moment@2.29.1\"></script>\n"
 "    <script src=\"https://cdn.jsdelivr.net/npm/chartjs-adapter-moment@0.1.1\"></script>\n"
+"    <link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x\" crossorigin=\"anonymous\">\n"
+"    <script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js\" integrity=\"sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4\" crossorigin=\"anonymous\"></script>\n"
 "</body>\n"
 "</html>";
