9254eb3c24f614405fb533c313050aa52f9a941a
Chaz George <chaz2x4@users.noreply.github.com>
2021-12-17 11:16:30 -0800

    Reduce cycleTime and other code cleanup

--- lib/GCP/GCP.h
+++ lib/GCP/GCP.h
@@ -64,8 +64,8 @@ private:
     void parseQueue(ulong);
 
     double currentTemp;
-    double tempOffset = -6.0;
-    double targetTemp = 98.0;
+    double tempOffset = -8.5;
+    double targetTemp = 92.0;
     double targetSteamTemp = 150.0;
 
     const double emergencyShutoffTemp = 165.0;
@@ -75,7 +75,7 @@ private:
     const double minSteamTemp = 140.0;
     const double maxOffset = 15;
     const double minOffset = -15;
-    const int websiteQueueSize = 60;
+    const int websiteQueueSize = 90;
 
     double brew_output;
     double steam_output;
