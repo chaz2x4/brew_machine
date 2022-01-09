71fb1ae1f417fc719376f5a6b363c3a1975dd242
Chaz George <chaz2x4@users.noreply.github.com>
2022-01-08 17:24:34 -0800

    Start complete unit test of GCP function

--- test/test_gcp.cpp
+++ test/test_gcp.cpp
@@ -46,32 +46,58 @@ void test_function_gcp_decrement_steam_temp(){
     TEST_ASSERT_EQUAL(149, brew_machine.getTargetTemp("steam"));
 }
 
-void test_function_gcp_brew_max(){
+void test_function_gcp_brew_max_1(){
     brew_machine.setTargetTemp("brew", 162);
     TEST_ASSERT_EQUAL(115, brew_machine.getTargetTemp("brew"));
+}
+
+void test_function_gcp_brew_max_2(){
+    brew_machine.setTargetTemp("brew", 162);
     brew_machine.incrementTemp("brew");
     TEST_ASSERT_EQUAL(115, brew_machine.getTargetTemp("brew"));
 }
 
-void test_function_gcp_brew_min(){
+void test_function_gcp_brew_min_1(){
     brew_machine.setTargetTemp("brew", 0);
     TEST_ASSERT_EQUAL(85, brew_machine.getTargetTemp("brew"));
+}
+
+void test_function_gcp_brew_min_2(){
+    brew_machine.setTargetTemp("brew", 0);
     brew_machine.decrementTemp("brew");
     TEST_ASSERT_EQUAL(85, brew_machine.getTargetTemp("brew"));
 }
 
-void test_function_gcp_steam_max(){
+void test_function_gcp_steam_max_1(){
     brew_machine.setTargetTemp("steam", 162);
     TEST_ASSERT_EQUAL(160, brew_machine.getTargetTemp("steam"));
+}
+
+void test_function_gcp_steam_max_2(){
+    brew_machine.setTargetTemp("steam", 162);
     brew_machine.incrementTemp("steam");
     TEST_ASSERT_EQUAL(160, brew_machine.getTargetTemp("steam"));
 }
 
-void test_function_gcp_steam_min(){
+void test_function_gcp_steam_min_1(){
     brew_machine.setTargetTemp("steam", 0);
     TEST_ASSERT_EQUAL(140, brew_machine.getTargetTemp("steam"));
+}
+
+void test_function_gcp_steam_min_2(){
+    brew_machine.setTargetTemp("steam", 0);
     brew_machine.decrementTemp("steam");
     TEST_ASSERT_EQUAL(140, brew_machine.getTargetTemp("steam"));
+}
+
+void test_function_gcp_offset_max(){
+    brew_machine.setTempOffset(20);
+    TEST_ASSERT_EQUAL(15, brew_machine.getTempOffset());
+}
+
+void test_function_gcp_offset_min(){
+    brew_machine.setTempOffset(-20);
+    TEST_ASSERT_EQUAL(-15, brew_machine.getTempOffset());
 }
 
 void test_function_gcp_current_temp(){
@@ -83,6 +109,41 @@ void test_function_gcp_current_temp(){
 void test_function_gcp_set_tunings(){
     brew_machine.setTunings(10, 10, 10);
     TEST_ASSERT_EQUAL_STRING("{ \"kp\": 10.00, \"ki\": 10.00, \"kd\": 10.00 }", brew_machine.getTunings().c_str());
+}
+
+void test_function_gcp_eeprom_brew(){
+    double temp;
+    brew_machine.setTargetTemp("brew", 96);
+    EEPROM.get(BREW_TEMP_ADDRESS, temp);
+    TEST_ASSERT_EQUAL(96, temp);
+}
+
+void test_function_gcp_eeprom_steam(){
+    double temp;
+    brew_machine.setTargetTemp("steam", 152);
+    EEPROM.get(STEAM_TEMP_ADDRESS, temp);
+    TEST_ASSERT_EQUAL(152, temp);
+}
+
+void test_function_gcp_eeprom_offset(){
+    double temp;
+    brew_machine.setTempOffset(-11);
+    EEPROM.get(OFFSET_ADDRESS, temp);
+    TEST_ASSERT_EQUAL(-11, temp);
+}
+
+void test_function_gcp_eeprom_tunings(){
+    double actualTunings[3];
+    double expectedTunings[3] = {11, 11, 11};
+    brew_machine.setTunings(11, 11, 11);
+    for(int i=0; i<3 ; i++) {
+        EEPROM.get(TUNING_ADDRESS + i*8, actualTunings[i]);
+    }
+    TEST_ASSERT_EQUAL(expectedTunings, actualTunings);
+}
+
+void test_function_gcp_output(){
+
 }
 
 void setup(){
@@ -97,12 +158,23 @@ void setup(){
     RUN_TEST(test_function_gcp_decrement_brew_temp);
     RUN_TEST(test_function_gcp_increment_steam_temp);
     RUN_TEST(test_function_gcp_decrement_steam_temp);
-    RUN_TEST(test_function_gcp_brew_max);
-    RUN_TEST(test_function_gcp_brew_min);
-    RUN_TEST(test_function_gcp_steam_max);
-    RUN_TEST(test_function_gcp_steam_min);
+    RUN_TEST(test_function_gcp_brew_max_1);
+    RUN_TEST(test_function_gcp_brew_min_1);
+    RUN_TEST(test_function_gcp_steam_max_1);
+    RUN_TEST(test_function_gcp_steam_min_1);
+    RUN_TEST(test_function_gcp_brew_max_2);
+    RUN_TEST(test_function_gcp_brew_min_2);
+    RUN_TEST(test_function_gcp_steam_max_2);
+    RUN_TEST(test_function_gcp_steam_min_2);
+    RUN_TEST(test_function_gcp_offset_max);
+    RUN_TEST(test_function_gcp_offset_min);
     RUN_TEST(test_function_gcp_current_temp);
     RUN_TEST(test_function_gcp_set_tunings);
+    RUN_TEST(test_function_gcp_eeprom_brew);
+    RUN_TEST(test_function_gcp_eeprom_steam);
+    RUN_TEST(test_function_gcp_eeprom_offset);
+    RUN_TEST(test_function_gcp_eeprom_tunings);
+    RUN_TEST(test_function_gcp_output);
     UNITY_END();
 }
 
