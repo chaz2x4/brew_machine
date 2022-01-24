d830c8b3ac683984b4dd3b484fbeed60979e1a40
Chaz George <chaz2x4@users.noreply.github.com>
2022-01-24 02:25:15 -0800

    Remove fault check for speed lol

--- lib/gcp/gcp.cc
+++ lib/gcp/gcp.cc
@@ -141,17 +141,6 @@ double GCP::getPressure() {
 
 double GCP::getActualTemp() {
 	double temp =  tempProbe.temperature(100, RREF);
-	uint8_t fault = tempProbe.readFault();
-	if (fault) {
-		Serial.print("Fault 0x"); Serial.println(fault, HEX);
-		if (fault & MAX31865_FAULT_HIGHTHRESH) Serial.println("RTD High Threshold");
-		if (fault & MAX31865_FAULT_LOWTHRESH) Serial.println("RTD Low Threshold"); 
-		if (fault & MAX31865_FAULT_REFINLOW) Serial.println("REFIN- > 0.85 x Bias");
-		if (fault & MAX31865_FAULT_REFINHIGH) Serial.println("REFIN- < 0.85 x Bias - FORCE- open");
-		if (fault & MAX31865_FAULT_RTDINLOW) Serial.println("RTDIN- < 0.85 x Bias - FORCE- open");
-		if (fault & MAX31865_FAULT_OVUV) Serial.println("Under/Over voltage");
-		tempProbe.clearFault();
-	}
 	return temp;
 }
 
