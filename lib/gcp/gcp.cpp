310542c2f4fea452485f470a35f406001193850c
Chaz George <chaz2x4@users.noreply.github.com>
2022-10-10 19:52:45 -0700

    Make emergency cutoff even more conservative

--- lib/gcp/gcp.cpp
+++ lib/gcp/gcp.cpp
@@ -2,7 +2,7 @@
 
 GCP::GCP()
 : tempProbe(Adafruit_MAX31865(THERMOPROBE_PIN))
-, kEmergencyShutoffTemp(165.0)
+, kEmergencyShutoffTemp(155.0)
 , kMaxBrewTemp(105.0)
 , kMinBrewTemp(85.0)
 , kMaxSteamTemp(150.0)
